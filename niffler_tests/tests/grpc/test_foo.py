42772fc85a157fdd28d453116c1ad8b94b40863b
atrskv <legaterayne@gmail.com>
2026-03-16 22:06:05 +0300

    feat: add grpc client

---
+++ niffler_tests/tests/grpc/test_foo.py
@@ -0,0 +1,10 @@
+from google.protobuf import empty_pb2
+from internal.clients.pb.niffler_currency_pb2_pbreflect import (
+    NifflerCurrencyServiceClient,
+)
+
+
+def test_get_all_currencies(grpc_client: NifflerCurrencyServiceClient):
+    response = grpc_client.get_all_currencies(empty_pb2.Empty())
+    # assert response.allCurrencies
+    print(response)
