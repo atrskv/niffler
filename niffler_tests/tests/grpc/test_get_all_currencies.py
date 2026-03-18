4bd63f4a7ff87ac63e4872d4abbb51317703bd4b
atrskv <legaterayne@gmail.com>
2026-03-18 19:10:38 +0300

    test: add grpc tests

--- niffler_tests/tests/grpc/test_get_all_currencies.py
+++ niffler_tests/tests/grpc/test_get_all_currencies.py
@@ -1,9 +1,24 @@
 from google.protobuf import empty_pb2
+from internal.clients.pb.niffler_currency_pb2 import CurrencyValues
 from internal.clients.pb.niffler_currency_pb2_pbreflect import (
     NifflerCurrencyServiceClient,
 )
 
 
-def test_get_all_currencies(grpc_client: NifflerCurrencyServiceClient):
+def test_get_all_currencies(
+    grpc_client: NifflerCurrencyServiceClient,
+):
+    expected = {
+        CurrencyValues.RUB,
+        CurrencyValues.USD,
+        CurrencyValues.EUR,
+        CurrencyValues.KZT,
+    }
+
     response = grpc_client.get_all_currencies(empty_pb2.Empty())
+
     assert len(response.allCurrencies) == 4
+    currencies = {c.currency: c.currencyRate for c in response.allCurrencies}
+    assert set(currencies.keys()) == expected
+    for rate in currencies.values():
+        assert rate > 0, f"Currency rate {rate} should be positive"
