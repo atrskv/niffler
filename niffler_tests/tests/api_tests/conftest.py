1cce05a8f31b1de62783fc861b1f4ee08ce406c6
atrskv <legaterayne@gmail.com>
2026-03-31 22:54:37 +0300

    test: add soap templates

---
+++ niffler_tests/tests/api_tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture
+def rollback_user(users_client, user):
+    original = user.model_copy()
+
+    yield
+
+    users_client.update_user(original)
+
+
+@pytest.fixture
+def rollback_spends(spends_client):
+    created_spends = []
+
+    yield created_spends
+
+    if created_spends:
+        spend_ids = [spend.id for spend in created_spends]
+        spends_client.delete_spends(spend_ids)
