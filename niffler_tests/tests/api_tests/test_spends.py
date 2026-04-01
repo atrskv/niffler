34702a0e9d20dbdc249542cd0d0aaa8334bd5ae8
atrskv <legaterayne@gmail.com>
2026-04-01 19:07:58 +0300

    refactor: update api tests

--- niffler_tests/tests/api_tests/test_spends.py
+++ niffler_tests/tests/api_tests/test_spends.py
@@ -2,29 +2,25 @@ import allure
 import pytest
 from requests.exceptions import HTTPError
 
-from internal.data.models.spend import SpendAddAPI
-
 
 @allure.story("Adding")
 class TestAdding:
-    def test_add_spend(
-        self, spends_client, as_a_registered_user, rollback_spends, category
+    def test_add_spend(self, spends_client, as_a_registered_user, spend_payload):
+        created_spend = spends_client.add_spend(
+            spend_payload, as_a_registered_user.username
+        )
+
+        assert created_spend.amount == spend_payload.amount
+        assert created_spend.description == spend_payload.description
+        assert created_spend.currency == spend_payload.currency
+
+    def test_add_spend_with_invalid_amount(
+        self, spends_client, as_a_registered_user, spend_payload
     ):
-        spend_data = SpendAddAPI.random(category_name=category.name)
-
-        created = spends_client.add_spend(spend_data, as_a_registered_user.username)
-
-        assert created.amount == spend_data.amount
-        assert created.description == spend_data.description
-        assert created.currency == spend_data.currency
-        rollback_spends.append(created)
-
-    def test_add_spend_with_invalid_amount(self, spends_client, as_a_registered_user):
-        spend_data = SpendAddAPI.random()
-        spend_data.amount = "abc"
+        spend_payload.amount = "abc"
 
         with pytest.raises(HTTPError) as e:
-            spends_client.add_spend(spend_data, as_a_registered_user.username)
+            spends_client.add_spend(spend_payload, as_a_registered_user.username)
 
         assert e.value.response.status_code == 400
         assert "Failed to read request" in e.value.response.text
@@ -33,46 +29,38 @@ class TestAdding:
 @allure.story("Updating")
 class TestUpdating:
     def test_update_spend_already_deleted(
-        self, spends_client, as_a_registered_user, category
+        self, spends_client, as_a_registered_user, created_spend
     ):
-        spend_data = SpendAddAPI.random(category_name=category.name)
-        created = spends_client.add_spend(spend_data, as_a_registered_user.username)
-        new_amount = created.amount = 99.0
+        spends_client.delete_spends([created_spend.id])
+        created_spend.amount = 99.0
 
-        spends_client.delete_spends([created.id])
-        created.amount = new_amount
-        with pytest.raises(Exception) as e:
-            spends_client.edit_spend(created)
+        with pytest.raises(HTTPError) as e:
+            spends_client.edit_spend(created_spend)
 
         assert e.value.response.status_code == 404
         assert "Can`t find spend by given id" in e.value.response.text
 
-    def test_update_spend(
-        self, spends_client, as_a_registered_user, rollback_spends, category
+    def test_update_spend_amount_and_preserve_fields(
+        self, spends_client, created_spend
     ):
-        spend_data = SpendAddAPI.random(category_name=category.name)
-        created = spends_client.add_spend(spend_data, as_a_registered_user.username)
-        new_amount = created.amount = 99.0
-        created.amount = new_amount
+        expected_amount = 99.0
+        created_spend.amount = expected_amount
 
-        edited = spends_client.edit_spend(created)
+        updated_spend = spends_client.edit_spend(created_spend)
 
-        assert edited.amount == new_amount
-        assert edited.description == created.description
-        assert edited.currency == created.currency
-        rollback_spends.append(edited)
+        assert updated_spend.amount == expected_amount
+        assert updated_spend.description == created_spend.description
+        assert updated_spend.currency == created_spend.currency
 
 
 @allure.story("Deleting")
 class TestDeleting:
-    def test_delete_spend(self, spends_client, category, as_a_registered_user):
-        spend_data = SpendAddAPI.random(category_name=category.name)
-
-        created = spends_client.add_spend(spend_data, as_a_registered_user.username)
-        spends_client.delete_spends([created.id])
+    def test_delete_spend(self, spends_client, created_spend):
+        spends_client.delete_spends([created_spend.id])
 
         all_spends = spends_client.get_spends()
-        assert not any(s.id == created.id for s in all_spends)
+
+        assert not any(s.id == created_spend.id for s in all_spends)
 
 
 pytestmark = [
