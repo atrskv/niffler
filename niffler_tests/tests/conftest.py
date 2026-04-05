2314cfeb9bde123803cd35f4166ec48f8f93616d
atrskv <legaterayne@gmail.com>
2026-04-05 15:07:04 +0300

    feat: add remote run

--- niffler_tests/tests/conftest.py
+++ niffler_tests/tests/conftest.py
@@ -5,7 +5,7 @@ from allure_commons.types import AttachmentType
 from allure_pytest.listener import AllureListener
 from pytest import FixtureDef, FixtureRequest, Item
 from selene import browser
-
+from selenium.webdriver.chrome.options import Options
 from internal import settings
 from internal.clients.api.auth import AuthClient
 from internal.interceptors.allure import AllureInterceptor
@@ -51,6 +51,27 @@ def pytest_fixture_setup(fixturedef: FixtureDef, request: FixtureRequest):
 def in_browser():
     browser.config.base_url = settings.config.frontend_url
     browser.config.timeout = settings.config.timeout
+
+    if settings.config.context == 'remote':
+        selenoid_capabilities = {
+            'browserName': settings.config.driver_name,
+            'browserVersion': settings.config.driver_version,
+            'selenoid:options': {
+                'enableVNC': settings.config.enable_VNC,
+                'enableVideo': settings.config.enable_video
+            },
+        }
+
+        options = Options()
+        options.capabilities.update(selenoid_capabilities)
+
+        browser.config.driver_remote_url = f"{settings.config.selenoid_url}/wd/hub"
+        browser.config.driver_options = options
+
+        options.add_experimental_option(
+            "prefs",
+            {"intl.accept_languages": "ru,ru_RU"}
+        )
 
     yield
 
